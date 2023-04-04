03b33bc06d49d0e1ee4f25181855372c6f2de1fe
guiweb85@gmail.com <guiweb85@gmail.com>
2023-04-03 22:53:58 -0300

    353. Consulta simples com @Query

--- src/main/java/com/guilhermemendes/workshopspringbootmongodb/repository/PostRepository.java
+++ src/main/java/com/guilhermemendes/workshopspringbootmongodb/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.guilhermemendes.workshopspringbootmongodb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.guilhermemendes.workshopspringbootmongodb.domain.Post;
 
@@ -8,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
