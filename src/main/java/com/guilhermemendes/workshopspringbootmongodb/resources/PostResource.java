b48ec772887545d26fc86cfb7e06230f38e9851d
guiweb85@gmail.com <guiweb85@gmail.com>
2023-04-04 22:41:43 -0300

    354. Consulta com vários critérios

--- src/main/java/com/guilhermemendes/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/guilhermemendes/workshopspringbootmongodb/resources/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,6 +32,18 @@ public class PostResource {
         return ResponseEntity.ok().body(list);
     }
 
+    @RequestMapping(value="/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue = "") String text,
+            @RequestParam(value="minDate", defaultValue = "") String minDate,
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
 
 
 
--- src/main/java/com/guilhermemendes/workshopspringbootmongodb/services/PostService.java
+++ src/main/java/com/guilhermemendes/workshopspringbootmongodb/services/PostService.java
@@ -9,6 +9,7 @@ import com.guilhermemendes.workshopspringbootmongodb.services.exception.ObjectNo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 
 }
